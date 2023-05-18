1f0c8013581ada533410a9fb9f14f0c649ddd504
NMarkhotsky <NMarkhotsky@users.noreply.github.com>
2023-05-18 16:06:37 +0000

    deploy: 289a036ee5f6cba4479899476ed7b124faddb525

---
+++ assets/Home-231ccaec.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-2f43694e.js";function r(){return e.jsx("main",{children:"Home"})}export{r as default};
--- assets/Tweets-c15ca845.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-9a6e8c5d.js";function r(){return e.jsx("main",{children:"Tweets"})}export{r as default};
