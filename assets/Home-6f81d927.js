4155eaa77b390d936b2e7083209a1f70ba056475
NMarkhotsky <NMarkhotsky@users.noreply.github.com>
2023-05-20 18:36:50 +0000

    deploy: b3f6580c7c7ab7853e9d318e10b6927b57245c47

---
+++ assets/Home-6f81d927.js
@@ -0,0 +1,5 @@
+import{s,j as t}from"./index-003b9e8a.js";const e=s.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`,i=s.img``,n="/tech-tweets-task/assets/bird-32692a58.svg";function a(){return t.jsx(e,{children:t.jsx(i,{src:n,width:"50%",alt:"bird"})})}export{a as default};
--- assets/Home-a1c23dcb.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-0f3babe8.js";function r(){return e.jsx("main",{children:"Home"})}export{r as default};
