cbefb26bfdeeb0ec4f5a4b9330c9eacf452f429e
NMarkhotsky <NMarkhotsky@users.noreply.github.com>
2023-05-21 18:26:28 +0000

    deploy: 2bf155ae09fe2ffe92acad356d403148172fac37

--- assets/Home-d06352c1.js
+++ assets/Home-d06352c1.js
@@ -1,5 +1,7 @@
-import{s,j as t}from"./index-0c12fbb7.js";const e=s.main`
+import{s,j as t}from"./index-4a30905a.js";const e=s.main`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  min-height: 70vh;
 `,i=s.img``,n="/tech-tweets-task/assets/bird-32692a58.svg";function a(){return t.jsx(e,{children:t.jsx(i,{src:n,width:"50%",alt:"bird"})})}export{a as default};
